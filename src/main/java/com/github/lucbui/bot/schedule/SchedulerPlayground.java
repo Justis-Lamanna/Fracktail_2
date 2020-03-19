6cfb24a44a793a808aa40b5c11fce628014060ee
Justis Lamanna <lamanna.justis@gmail.com>
2020-03-18 21:31:18 -0500

    Only tell me to sleep when I'm online

--- src/main/java/com/github/lucbui/bot/schedule/SchedulerPlayground.java
+++ src/main/java/com/github/lucbui/bot/schedule/SchedulerPlayground.java
@@ -1,21 +1,23 @@
 package com.github.lucbui.bot.schedule;
 
+import com.github.lucbui.bot.Constants;
 import com.github.lucbui.magic.schedule.SchedulerService;
 import com.github.lucbui.magic.schedule.cron.Cron;
 import com.github.lucbui.magic.util.DiscordUtils;
 import discord4j.core.DiscordClient;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.presence.Status;
 import discord4j.core.object.util.Snowflake;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalTime;
 
 @Service
 public class SchedulerPlayground {
-    public static final Snowflake LUCBUILAND_GUILD_ID = Snowflake.of("423976318082744321");
-    public static final Snowflake BOT_CHANNEL_ID = Snowflake.of("424562931489964032");
-    public static final Snowflake LUCBUI_ID = Snowflake.of("248612704019808258");
     @Autowired
     private SchedulerService schedulerService;
 
@@ -24,13 +26,16 @@ public class SchedulerPlayground {
 
     @PostConstruct
     private void scheduleThings() {
-        Cron at10PM = new Cron.Builder().everyDayAt(22, 0, 0).build();
+        Cron atBedtime = new Cron.Builder().everyDayAt(22, 0, 0).build();
         schedulerService.scheduleJob("bedtime", () -> {
-            bot.getGuildById(LUCBUILAND_GUILD_ID)
-                    .flatMap(guild -> guild.getChannelById(BOT_CHANNEL_ID))
+            bot.getGuildById(Constants.LUCBUILAND_GUILD_ID)
+                    .filterWhen(guild -> guild.getMemberById(Constants.LUCBUI_ID)
+                            .flatMap(Member::getPresence)
+                            .map(presence -> presence.getStatus() != Status.OFFLINE))
+                    .flatMap(guild -> guild.getChannelById(Constants.BOT_CHANNEL_ID))
                     .cast(TextChannel.class)
-                    .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(LUCBUI_ID) + ", GO THE HECK TO SLEEP"))
+                    .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(Constants.LUCBUI_ID) + ", GO THE HECK TO SLEEP"))
                     .block();
-        }, at10PM);
+        }, atBedtime);
     }
 }
