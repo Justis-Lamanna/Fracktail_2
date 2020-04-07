dd5e35a992df13583ebe67b44a7a7663300c9cb8
Justis Lamanna <lamanna.justis@gmail.com>
2020-04-07 11:41:29 -0500

    Ping Snowpaws at bedtime

--- src/main/java/com/github/lucbui/bot/schedule/SchedulerPlayground.java
+++ src/main/java/com/github/lucbui/bot/schedule/SchedulerPlayground.java
@@ -6,6 +6,7 @@ import com.github.lucbui.bot.services.channel.BotChannelService;
 import com.github.lucbui.magic.util.DiscordUtils;
 import discord4j.core.DiscordClient;
 import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Permission;
 import discord4j.core.object.util.Snowflake;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,8 @@ import java.util.stream.Collectors;
 @Component
 public class SchedulerPlayground {
     private static final Snowflake LUCBUI_ID = Snowflake.of("248612704019808258");
-    public static final String BOT_CHANNEL_ID = "424562931489964032";
+    public static final Snowflake BOT_CHANNEL_ID = Snowflake.of("424562931489964032");
+    public static final Snowflake SNEPPY_ID = Snowflake.of("112010148687437824");
 
     @Autowired
     private BotChannelService botChannelService;
@@ -33,10 +35,18 @@ public class SchedulerPlayground {
 
     @Scheduled(cron = "0 0 22 * * SUN-THU")
     public void scheduleBedtimePing() {
-        bot.getChannelById(Snowflake.of(BOT_CHANNEL_ID))
-            .cast(TextChannel.class)
-            .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(LUCBUI_ID) + ", GO THE HECK TO SLEEP"))
-            .block();
+        bot.getChannelById(BOT_CHANNEL_ID)
+                .cast(TextChannel.class)
+                .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(LUCBUI_ID) + ", GO THE HECK TO SLEEP"))
+                .block();
+    }
+
+    @Scheduled(cron = "0 0 23 * * SUN-THU", zone = "Europe/London")
+    public void scheduleSneppyBedtimePing() {
+        bot.getUserById(SNEPPY_ID)
+                .flatMap(User::getPrivateChannel)
+                .flatMap(pc -> pc.createMessage(DiscordUtils.getMentionFromId(SNEPPY_ID) + ", Go to sleep or I take your tail!"))
+                .block();
     }
 
     @Scheduled(cron = "0 0 0 * * *")
