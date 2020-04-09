0225dc592fb8b9a222865f1db787d4e004237911
Justis Lamanna <lamanna.justis@gmail.com>
2020-04-08 21:28:08 -0500

    Fixes and enhancements to calendar commands

--- src/main/java/com/github/lucbui/bot/cmds/CalendarCommands.java
+++ src/main/java/com/github/lucbui/bot/cmds/CalendarCommands.java
@@ -5,6 +5,7 @@ import com.github.lucbui.bot.services.calendar.CalendarService;
 import com.github.lucbui.magic.annotation.*;
 import com.github.lucbui.magic.exception.CommandValidationException;
 import com.github.lucbui.magic.util.DiscordUtils;
+import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Snowflake;
@@ -15,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,7 +24,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
-import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -37,6 +36,9 @@ public class CalendarCommands {
 
     @Autowired
     private CalendarService calendarService;
+
+    @Autowired
+    private DiscordClient bot;
 
     @Command(help = "Get the next birthday, or birthdays, coming up. Optionally, specify a number between 1 and 10 to get the next n birthdays.")
     public Mono<String> nextbirthday(@Param(0) OptionalInt in) {
@@ -63,7 +65,7 @@ public class CalendarCommands {
     }
 
     @Command(help = "Get all birthdays occuring today.")
-    public Mono<String> todaysbirthdays() throws IOException {
+    public Mono<String> todaysbirthdays() {
         return calendarService.getTodaysBirthday()
                 .collectList()
                 .map(birthdays -> {
@@ -92,7 +94,7 @@ public class CalendarCommands {
     }
 
     @Command(help = "Get all birthdays occuring this month.")
-    public Mono<String> monthsbirthdays(@Param(0) String monthStr) throws IOException {
+    public Mono<String> monthsbirthdays(@Param(0) String monthStr) {
         return Mono.justOrEmpty(monthStr)
             .map(this::validateAndConvertMonth)
             .switchIfEmpty(Mono.fromSupplier(() -> LocalDate.now().getMonth()))
@@ -117,7 +119,7 @@ public class CalendarCommands {
 
 
     @Command(help = "Get the birthday of a specific user. Usage is !birthday [user's name or @].")
-    public Mono<String> birthday(@Param(0) String user, @BasicSender User sender) throws IOException {
+    public Mono<String> birthday(@Param(0) String user, @BasicSender User sender) {
         return Mono.justOrEmpty(user)
                 .flatMap(userParam -> {
                     Optional<String> userIdIfPresent = DiscordUtils.getIdFromMention(userParam);
@@ -132,7 +134,7 @@ public class CalendarCommands {
                 .defaultIfEmpty("Sorry, I don't know when " + (user == null ? "your" : user + "'s") + " birthday is.");
     }
 
-    private <R> LocalDate validateAndConvertDate(String date) {
+    private LocalDate validateAndConvertDate(String date) {
         try {
             LocalDate dateOfBirth = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
             int currentYear = LocalDate.now().get(ChronoField.YEAR);
@@ -143,18 +145,18 @@ public class CalendarCommands {
             }
             return dateOfBirth;
         } catch (DateTimeParseException ex) {
-            throw new CommandValidationException("Correct usage: !addbirthday [yyyy-mm-dd]");
+            throw new CommandValidationException("Date provided was invalid, should be formatted as [yyyy-mm-dd]");
         }
     }
 
     @Command(help = "Add a user's birthday. Usage is !addbirthday [yyyy-mm-dd]")
-    public Mono<String> addbirthday(@BasicSender User sender, @Param(0) String date) throws IOException {
+    public Mono<String> addbirthday(@BasicSender User sender, @Param(0) String date) {
         if(date == null) {
             return Mono.just("Correct usage: !addbirthday [yyyy-mm-dd]");
         }
 
         return calendarService.searchBirthdayById(sender.getId())
-                .flatMap(bday -> Mono.error(new IllegalArgumentException(String.format("You already have a birthday set to %tD.", bday.getDate()))))
+                .flatMap(bday -> Mono.error(new CommandValidationException(String.format("You already have a birthday set to %tD.", bday.getDate()))))
                 .then(Mono.just(date))
                 .map(this::validateAndConvertDate)
                 .map(bday -> new Birthday(
@@ -162,44 +164,30 @@ public class CalendarCommands {
                         StringUtils.capitalize(sender.getUsername()),
                         bday))
                 .flatMap(calendarService::addBirthday)
-                .then(Mono.just("Added " + sender.getUsername() + "'s birthday to the birthday calendar"))
-                .onErrorResume(IllegalArgumentException.class, ex -> Mono.just(ex.getMessage()));
+                .then(Mono.just("Added " + sender.getUsername() + "'s birthday to the birthday calendar"));
     }
-//
-//    @Command(help = "Add a user's birthday. Usage is !setbirthday [user-snowflake] [yyyy-mm-dd]")
-//    @Permissions("admin")
-//    public Mono<Void> setbirthday(MessageCreateEvent event, @Param(0) String userId, @Param(1) String date) throws IOException {
-//        LocalDate dateOfBirth;
-//        try {
-//            dateOfBirth = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
-//            int currentYear = LocalDate.now().get(ChronoField.YEAR);
-//            if(dateOfBirth.get(ChronoField.YEAR) < OLDEST_POSSIBLE_YEAR) {
-//                return DiscordUtils.respond(event.getMessage(), "They are definitely not that old.");
-//            } else if(dateOfBirth.get(ChronoField.YEAR) > currentYear) {
-//                return DiscordUtils.respond(event.getMessage(), "They were not born in the future, either");
-//            }
-//        } catch (DateTimeParseException ex) {
-//            return DiscordUtils.respond(event.getMessage(), "Correct usage: !setbirthday [user-snowflake] [yyyy-mm-dd]");
-//        }
-//
-//        Snowflake requestedId = DiscordUtils.getIdFromMention(userId).map(Snowflake::of).orElse(Snowflake.of(userId));
-//
-//        return event.getClient()
-//                .getUserById(requestedId)
-//                .flatMap(user -> {
-//                    Birthday birthday = new Birthday(
-//                            userId,
-//                            StringUtils.capitalize(user.getUsername()),
-//                            dateOfBirth);
-//                    try {
-//                        calendarService.addBirthday(birthday);
-//                        return DiscordUtils.respond(event.getMessage(), "Added " + user.getUsername() + "'s birthday as " + date + ".");
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                        return DiscordUtils.respond(event.getMessage(), "There was an error. Birthday was not added.");
-//                    }
-//                });
-//    }
+
+    @Command(help = "Add a user's birthday. Usage is !setbirthday [user-snowflake] [yyyy-mm-dd]")
+    @Permissions("admin")
+    public Mono<String> setbirthday(@Param(0) String userId, @Param(1) String date) {
+        if(userId == null || date == null) {
+            return Mono.just("Correct usage: !setbirthday [user-snowflake] [yyyy-mm-dd]");
+        }
+
+        Mono<User> userMono = Mono.just(userId)
+                .map(id -> DiscordUtils.getIdFromMention(id).map(Snowflake::of).orElse(Snowflake.of(id)))
+                .flatMap(snowflake -> bot.getUserById(snowflake));
+        Mono<LocalDate> birthdayMono = Mono.just(date)
+                .map(this::validateAndConvertDate);
+
+        return Mono.zip(userMono, birthdayMono)
+                .map(userDate -> new Birthday(
+                        userDate.getT1().getId().asString(),
+                        StringUtils.capitalize(userDate.getT1().getUsername()),
+                        userDate.getT2()))
+                .flatMap(birthday -> calendarService.addBirthday(birthday).thenReturn(birthday))
+                .map(birthday -> String.format("Added %s's birthday as %tD.", birthday.getName(), birthday.getDate()));
+    }
 
     private String getBirthdayText(Birthday nextBirthday) {
         Duration duration = Duration.between(LocalDateTime.now(), nextBirthday.getDate().atStartOfDay());
