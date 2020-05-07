78081b2ea8b99379d051479053928b28575961f0
Justis Lamanna <lamanna.justis@gmail.com>
2020-05-07 10:20:45 -0500

    Added test command to investigate polling

--- src/main/java/com/github/lucbui/bot/cmds/BotUseCommands.java
+++ src/main/java/com/github/lucbui/bot/cmds/BotUseCommands.java
@@ -7,11 +7,19 @@ import com.github.lucbui.magic.command.func.BotCommand;
 import com.github.lucbui.magic.command.store.CommandStore;
 import com.github.lucbui.magic.util.DiscordUtils;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.event.domain.message.ReactionAddEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.util.Snowflake;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.stream.Collectors;
@@ -68,4 +76,54 @@ public class BotUseCommands {
         return DiscordUtils.respond(evt.getMessage(), translateService.getString("sleep.text"))
                 .then(evt.getClient().logout());
     }
+
+    @Command
+    @CommandParams(0)
+    @Permissions("owner")
+    public Mono<Void> logs(MessageCreateEvent evt) {
+        File file = new File("/home/pi/fracktail/logs/app.log");
+        if(file.exists()) {
+            return evt.getMessage().getChannel()
+                    .flatMap(mc -> mc.createMessage(messageCreateSpec -> {
+                        try {
+                            messageCreateSpec.setContent("Here are the files!");
+                            messageCreateSpec.addFile("app.log", new FileInputStream(file));
+                        } catch (FileNotFoundException e) {
+                            messageCreateSpec.setContent("Error attaching logs: " + e.getMessage());
+                        }
+                    }))
+                    .then();
+        }
+        return DiscordUtils.respond(evt.getMessage(), "I can't get the logs on this environment.");
+    }
+
+    @Command
+    @CommandParams(0)
+    public Mono<Void> butt(MessageCreateEvent evt) {
+        Snowflake origMessageUserId = evt.getMessage().getAuthor().map(User::getId).orElse(null);
+        if(origMessageUserId == null){
+            return Mono.empty();
+        }
+        return evt.getMessage().getChannel()
+                .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(origMessageUserId) + ", react to this message with \uD83C\uDF51 to proceed."))
+                .cache()
+                .repeat()
+                .zipWith(evt.getClient().getEventDispatcher().on(ReactionAddEvent.class))
+                .filter(tuple -> {
+                    Snowflake origChannel = tuple.getT1().getChannelId();
+                    Snowflake origMessage = tuple.getT1().getId();
+                    ReactionAddEvent reactionAddEvent = tuple.getT2();
+                    String rawUnicode = reactionAddEvent.getEmoji().asUnicodeEmoji().map(ReactionEmoji.Unicode::getRaw).orElse(null);
+                    return reactionAddEvent.getChannelId().equals(origChannel) &&
+                            reactionAddEvent.getMessageId().equals(origMessage) &&
+                            reactionAddEvent.getUserId().equals(origMessageUserId) &&
+                            "\uD83C\uDF51".equals(rawUnicode);
+                })
+                .next()
+                .flatMap(tuple -> tuple.getT2().getChannel()
+                        .flatMap(channel ->
+                            channel.createMessage("Nice butt, " + DiscordUtils.getMentionFromId(tuple.getT2().getUserId()) + "!")))
+                .then();
+    }
 }
+
