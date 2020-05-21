62ae5c84ea0b5349e01d48d8ccf1244daed04c42
Justis Lamanna <lamanna.justis@gmail.com>
2020-05-21 14:56:22 -0500

    Added stats command

--- src/main/java/com/github/lucbui/bot/cmds/BotUseCommands.java
+++ src/main/java/com/github/lucbui/bot/cmds/BotUseCommands.java
@@ -7,26 +7,34 @@ import com.github.lucbui.magic.command.context.CommandUseContext;
 import com.github.lucbui.magic.command.func.BotCommand;
 import com.github.lucbui.magic.command.store.CommandStore;
 import com.github.lucbui.magic.util.DiscordUtils;
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Component;
+import com.profesorfalken.jsensors.model.components.Components;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.event.domain.message.ReactionAddEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
-import discord4j.core.object.reaction.ReactionEmoji;
-import discord4j.core.object.util.Snowflake;
+import discord4j.core.spec.EmbedCreateSpec;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Commands
 public class BotUseCommands {
+    private static final Pattern AT_SIGN = Pattern.compile("@");
+    public static final float BYTES_IN_A_MB = 1048576f;
     @Autowired
     private CommandStore commandStore;
 
@@ -103,32 +111,94 @@ public class BotUseCommands {
     }
 
     @Command
-    @CommandParams(0)
-    public Mono<Void> butt(MessageCreateEvent evt) {
-        Snowflake origMessageUserId = evt.getMessage().getAuthor().map(User::getId).orElse(null);
-        if(origMessageUserId == null){
-            return Mono.empty();
-        }
+    @Permissions("owner")
+    public Mono<Void> stats(MessageCreateEvent evt) {
         return evt.getMessage().getChannel()
-                .flatMap(channel -> channel.createMessage(DiscordUtils.getMentionFromId(origMessageUserId) + ", react to this message with \uD83C\uDF51 to proceed."))
-                .cache()
-                .repeat()
-                .zipWith(evt.getClient().getEventDispatcher().on(ReactionAddEvent.class))
-                .filter(tuple -> {
-                    Snowflake origChannel = tuple.getT1().getChannelId();
-                    Snowflake origMessage = tuple.getT1().getId();
-                    ReactionAddEvent reactionAddEvent = tuple.getT2();
-                    String rawUnicode = reactionAddEvent.getEmoji().asUnicodeEmoji().map(ReactionEmoji.Unicode::getRaw).orElse(null);
-                    return reactionAddEvent.getChannelId().equals(origChannel) &&
-                            reactionAddEvent.getMessageId().equals(origMessage) &&
-                            reactionAddEvent.getUserId().equals(origMessageUserId) &&
-                            "\uD83C\uDF51".equals(rawUnicode);
-                })
-                .next()
-                .flatMap(tuple -> tuple.getT2().getChannel()
-                        .flatMap(channel ->
-                            channel.createMessage("Nice butt, " + DiscordUtils.getMentionFromId(tuple.getT2().getUserId()) + "!")))
+                .flatMapMany(c -> c.createMessage(spec -> {
+                    spec.setEmbed(embed -> {
+                        embed.setTitle(translateService.getString("stats.title"));
+                        embed.setColor(new Color(183, 20, 65));
+
+                        embed.addField(
+                                translateService.getString("stats.label.pid"),
+                                AT_SIGN.split(ManagementFactory.getRuntimeMXBean().getName())[0], true);
+                        embed.addField(
+                                translateService.getString("stats.label.arch"),
+                                Objects.toString(ManagementFactory.getOperatingSystemMXBean().getArch(), "?"), true);
+                        embed.addField(
+                                translateService.getString("stats.label.osName"),
+                                Objects.toString(ManagementFactory.getOperatingSystemMXBean().getName(), "?"), true);
+                        embed.addField(
+                                translateService.getString("stats.label.threadCount"),
+                                translateService.getFormattedString("stats.threadCount", ManagementFactory.getThreadMXBean().getThreadCount()), true);
+                        embed.addField(
+                                translateService.getString("stats.label.uptime"),
+                                translateService.getFormattedString("stats.uptime",
+                                    Duration.between(startTime, Instant.now()).getSeconds(),
+                                    ManagementFactory.getRuntimeMXBean().getUptime() / 1000), true);
+                        embed.addField(
+                                translateService.getString("stats.label.memory"),
+                                getMemoryText(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage()), true);
+
+                        Components components = JSensors.get.components();
+
+                        doIt(components.disks, embed,
+                                doIt(components.gpus, embed,
+                                        doIt(components.mobos, embed,
+                                                doIt(components.cpus, embed, 19))));
+
+                        embed.setTimestamp(Instant.now());
+                    });
+                }))
                 .then();
+    }
+
+    private String getMemoryText(MemoryUsage usage) {
+        return translateService.getFormattedString("stats.memory",
+                usage.getInit() / BYTES_IN_A_MB,
+                usage.getCommitted() / BYTES_IN_A_MB,
+                usage.getUsed() / BYTES_IN_A_MB,
+                usage.getMax() / BYTES_IN_A_MB
+                );
+    }
+
+    private int doIt(List<? extends Component> components, EmbedCreateSpec spec, int remainingFieldCount) {
+        int fieldCount = getFieldCount(components);
+        if(CollectionUtils.isNotEmpty(components) && fieldCount < remainingFieldCount) {
+            addFields(components, spec);
+            return remainingFieldCount - fieldCount;
+        } else {
+            return remainingFieldCount;
+        }
+    }
+
+    private int getFieldCount(List<? extends Component> components) {
+        int count = 0;
+        for(Component c : components) {
+            count += 1; //For title
+            if(!isComponentEmpty(c)) {
+                count += CollectionUtils.size(c.sensors.temperatures) + CollectionUtils.size(c.sensors.fans);
+            }
+        }
+        return count;
+    }
+
+    private void addFields(List<? extends Component> components, EmbedCreateSpec spec) {
+        for(Component c : components) {
+            spec.addField(c.getClass().getSimpleName().toUpperCase(), Objects.toString(c.name, "?"), false);
+            if(!isComponentEmpty(c)) {
+                c.sensors.temperatures.forEach(t -> {
+                    spec.addField(t.name, translateService.getFormattedString("stats.temp", t.value), true);
+                });
+                c.sensors.fans.forEach(f -> {
+                    spec.addField(f.name, translateService.getFormattedString("stats.fan", f.value), true);
+                });
+            }
+        }
+    }
+
+    private boolean isComponentEmpty(Component c) {
+        return CollectionUtils.isEmpty(c.sensors.temperatures) && CollectionUtils.isEmpty(c.sensors.fans);
     }
 }
 
--- src/main/java/com/github/lucbui/bot/services/UsageCommandListFallback.java
+++ src/main/java/com/github/lucbui/bot/services/UsageCommandListFallback.java
@@ -1,14 +1,12 @@
-package com.github.lucbui.bot.config;
+package com.github.lucbui.bot.services;
 
 import com.github.lucbui.bot.services.translate.TranslateHelper;
 import com.github.lucbui.bot.services.translate.TranslateService;
 import com.github.lucbui.magic.command.context.CommandUseContext;
 import com.github.lucbui.magic.command.func.BotCommand;
-import com.github.lucbui.magic.command.func.BotMessageBehavior;
 import com.github.lucbui.magic.command.store.CommandListFallback;
 import com.github.lucbui.magic.token.Tokens;
 import com.github.lucbui.magic.util.DiscordUtils;
-import discord4j.core.event.domain.message.MessageCreateEvent;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,14 +30,7 @@ public class UsageCommandListFallback implements CommandListFallback {
             return Mono.empty();
         } else {
             String officialName = otherCandidates.get(0).getName();
-            return Mono.just(new UsageBotCommand(officialName));
-        }
-    }
-
-    private class UsageBotCommand extends BotCommand {
-        public UsageBotCommand(String command) {
-            super(command,
-                    event -> DiscordUtils.respond(event.getMessage(), translateService.getString(TranslateHelper.usageKey(command))));
+            return Mono.just(new BotCommand(officialName, event -> DiscordUtils.respond(event.getMessage(), translateService.getString(TranslateHelper.usageKey(officialName)))));
         }
     }
 }
